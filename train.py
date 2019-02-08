9b54b0ea0f36eb193bfd806f881b818497116cc3
Jakub Powierza <jpowie01@gmail.com>
2019-02-08 20:14:15 +0100

    Improve training by swapping labels and add script for visualization

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 import numpy as np
@@ -12,22 +13,29 @@ import dataset
 import models
 
 
-EPOCHS = 1000
-DISCRIMINATOR_TRAINING_ITERATIONS = 1
-BATCH_SIZE = 256
-LEARNING_RATE = 0.0002
-DATALOADER_WORKERS = 8
-CHECKPOINTS_DIR = './checkpoints/'
-DATASET_DIR = './dataset/'
-USE_GPU = torch.cuda.is_available()
+# Parse configuration passed from the CLI
+parser = argparse.ArgumentParser(description='Training DCGAN on CelebA dataset')
+parser.add_argument('--epochs', '-e', type=int, default=200, help='Number of epochs')
+parser.add_argument('--discriminator-training-iterations', '-k', type=int, default=1,
+                    help='Number of iterations for discriminator training (K parameter)')
+parser.add_argument('--random-label-swap', '-s', type=float, default=0.05, help='Percentage of labels that will be swapped')
+parser.add_argument('--batch-size', '-b', type=int, default=256, help='Size of a single batch')
+parser.add_argument('--learning-rate', '-lr', type=float, default=0.0002, help='Learning rate for Adam')
+parser.add_argument('--dataloader-workers', '-w', type=int, default=8, help='Number of threads used by Data Loader')
+parser.add_argument('--checkpoints-directory', '-c', type=str, default='./checkpoints/',
+                    help='Directory where all models checkpoints will be collected')
+parser.add_argument('--dataset', '-d', type=str, default='./dataset/', help='Directory where dataset will be stored')
+parser.add_argument('--use-gpu', '-g', type=bool, default=torch.cuda.is_available(), help='Use GPU if True')
+configuration = parser.parse_args()
 
-device = torch.device("cuda" if USE_GPU else "cpu")
+# Prepare placeholder for device used by this script (CPU or GPU)
+DEVICE = torch.device('cuda' if configuration.use_gpu else 'cpu')
 
 # Prepare directory for models checkpoints
-os.makedirs(CHECKPOINTS_DIR, exist_ok=True)
+os.makedirs(configuration.checkpoints_directory, exist_ok=True)
 
 # Prepare dataset for training
-train_dataset, train_loader = dataset.get(BATCH_SIZE, DATASET_DIR, DATALOADER_WORKERS)
+train_dataset, train_loader = dataset.get(configuration.batch_size, configuration.dataset, configuration.dataloader_workers)
 number_of_training_examples = len(train_dataset)
 
 # Prepare our networks for the training process
@@ -36,16 +44,17 @@ generator.apply(models.Generator.weights_init)
 discriminator = models.Discriminator()
 discriminator.apply(models.Discriminator.weights_init)
 
-# Move models to the GPU
-generator.to(device)
-discriminator.to(device)
+# Move models to the GPU if needed
+generator.to(DEVICE)
+discriminator.to(DEVICE)
 
 # Prepare optimizers
 criterion = nn.BCELoss()
-generator_optimizer = torch.optim.Adam(generator.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
-discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+generator_optimizer = torch.optim.Adam(generator.parameters(), lr=configuration.learning_rate, betas=(0.5, 0.999))
+discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=configuration.learning_rate, betas=(0.5, 0.999))
 
-for epoch in range(EPOCHS):
+# Let's train our GAN!
+for epoch in range(configuration.epochs):
     batch = 0
     total_discriminator_loss = 0.0
     total_discriminator_ground_truth_accuracy = 0.0
@@ -53,11 +62,11 @@ for epoch in range(EPOCHS):
     total_generator_loss = 0.0
     train_loader_iterator = iter(train_loader)
 
-    with tqdm.tqdm(total=len(train_loader), desc=f'Epoch #{(epoch+1):02}/{EPOCHS:02}') as progress_bar:
+    with tqdm.tqdm(total=len(train_loader), desc=f'Epoch #{(epoch+1):02}/{configuration.epochs:02}') as progress_bar:
         end_of_training = False
         while not end_of_training:
             # Train discriminator first
-            for k in range(DISCRIMINATOR_TRAINING_ITERATIONS):
+            for k in range(configuration.discriminator_training_iterations):
                 discriminator_optimizer.zero_grad()
 
                 # Prepare batch of ground truth images
@@ -66,16 +75,18 @@ for epoch in range(EPOCHS):
                     end_of_training = True
                     break  # Now train the discriminator
                 current_batch_size = len(batch_of_images)
-                discriminated_ground_truth_images = discriminator(batch_of_images.to(device))  # TODO: This should probably be changed..
+                discriminated_ground_truth_images = discriminator(batch_of_images.to(DEVICE))
 
 		# Sample random noise and pass it to generate images
-                z = torch.normal(torch.zeros(current_batch_size, 100), torch.ones(current_batch_size, 100)).to(device)
+                z = torch.normal(torch.zeros(current_batch_size, 100), torch.ones(current_batch_size, 100)).to(DEVICE)
                 discriminated_generated_images = discriminator(generator(z))
 
                 # Calculate loss
-                ground_truth = torch.ones(current_batch_size, 1).to(device) * 0.1
+                ground_truth = torch.ones(current_batch_size, 1).to(DEVICE) * 0.1
+                ground_truth[torch.rand(current_batch_size) < configuration.random_label_swap] = 0.9  # Some of labels will be swapped
                 ground_truth_loss = criterion(discriminated_ground_truth_images, ground_truth)
-                generated = torch.ones(current_batch_size, 1).to(device) * 0.9
+                generated = torch.ones(current_batch_size, 1).to(DEVICE) * 0.9
+                generated[torch.rand(current_batch_size) < configuration.random_label_swap] = 0.1  # Some of labels will be swapped
                 generated_loss = criterion(discriminated_generated_images, generated)
                 discriminator_loss = ground_truth_loss + generated_loss
                 total_discriminator_ground_truth_accuracy += torch.sum(discriminated_ground_truth_images < 0.5).item()
@@ -90,11 +101,12 @@ for epoch in range(EPOCHS):
             generator_optimizer.zero_grad()
 
             # Sample random noise and pass it to generate images
-            z = torch.normal(torch.zeros(BATCH_SIZE, 100), torch.ones(BATCH_SIZE, 100)).to(device)
+            z = torch.normal(torch.zeros(configuration.batch_size, 100), torch.ones(configuration.batch_size, 100)).to(DEVICE)
             generated_images = generator(z)
             
             # Check how much the generator has fooled the discriminator
-            ground_truth = torch.ones(BATCH_SIZE, 1).to(device) * 0.1
+            ground_truth = torch.ones(configuration.batch_size, 1).to(DEVICE) * 0.1
+            ground_truth[torch.rand(configuration.batch_size) < configuration.random_label_swap] = 0.9  # Some of labels will be swapped
             generator_loss = criterion(discriminator(generated_images), ground_truth)
             total_generator_loss += generator_loss.item()
 
@@ -113,7 +125,7 @@ for epoch in range(EPOCHS):
     total_discriminator_ground_truth_accuracy /= number_of_training_examples
     total_discriminator_generated_accuracy /= number_of_training_examples
     total_generator_loss /= len(train_loader)
-    print(f'Epoch #{(epoch+1):02}/{EPOCHS:02}: Train D-Loss: {total_discriminator_loss:.4f} '
+    print(f'Epoch #{(epoch+1):02}/{configuration.epochs:02}: Train D-Loss: {total_discriminator_loss:.4f} '
           f'(GT Acc: {total_discriminator_ground_truth_accuracy*100:.4f}%, Gen Acc: {total_discriminator_generated_accuracy*100:.4f}%) '
           f'Train G-Loss: {total_generator_loss:.4f}\n')
 
@@ -125,4 +137,4 @@ for epoch in range(EPOCHS):
         'discriminator_optimizer_state_dict': discriminator_optimizer.state_dict(),
         'generator_loss': generator_loss,
         'discriminator_loss': discriminator_loss,
-    }, f'{CHECKPOINTS_DIR}checkpoint_e{(epoch+1):02}_{total_discriminator_loss:.4f}_{total_generator_loss:.4f}.torch')
+    }, f'{configuration.checkpoints_directory}checkpoint_e{(epoch+1):02}_{total_discriminator_loss:.4f}_{total_generator_loss:.4f}.torch')
---
+++ visualize.py
@@ -0,0 +1,97 @@
+import argparse
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import torch
+
+import dataset
+import models
+
+WIDTH = 8
+HEIGHT = 2
+
+# Parse configuration passed from the CLI
+parser = argparse.ArgumentParser(description='Visualize DCGAN on CelebA dataset')
+parser.add_argument('--checkpoint', '-c', type=str, required=True, help='Checkpoint which should be used for visualization')
+parser.add_argument('--dataset', '-d', type=str, default='./dataset/', help='Directory where dataset will be stored')
+parser.add_argument('--use-gpu', '-g', type=bool, default=torch.cuda.is_available(), help='Use GPU if True')
+parser.add_argument('--output-figure', '-o', type=str, default='figure', help='Output figure filename (without extension)')
+configuration = parser.parse_args()
+
+# Prepare placeholder for device used by this script (CPU or GPU)
+DEVICE = torch.device('cuda' if configuration.use_gpu else 'cpu')
+
+# Fetch dataset for comparison
+train_dataset, train_loader = dataset.get(WIDTH*HEIGHT, configuration.dataset, 1)
+
+# Load checkpoint
+checkpoint = torch.load(configuration.checkpoint, map_location=torch.device(DEVICE))
+
+# Load models from checkpoint
+generator = models.Generator()
+discriminator = models.Discriminator()
+generator.load_state_dict(checkpoint['generator_model_state_dict'])
+discriminator.load_state_dict(checkpoint['discriminator_model_state_dict'])
+
+# Move models to the GPU if needed
+generator.to(DEVICE)
+discriminator.to(DEVICE)
+
+# Generate some images
+z = torch.normal(torch.zeros(WIDTH*HEIGHT, 100), torch.ones(WIDTH*HEIGHT, 100)).to(DEVICE)
+generated_images = generator(z)
+generated_real_or_fake = discriminator(generated_images)
+
+# Preprocess generated images
+generated_images = generated_images.detach().numpy()
+images = generated_images - generated_images.min(axis=0)
+images /= generated_images.max(axis=0) - generated_images.min(axis=0)
+generated_images = np.transpose(images * 255.0, (0, 2, 3, 1)).astype(np.uint8)
+
+# Fetch batch of original images
+original_images, _ = next(iter(train_loader))
+original_real_or_fake = discriminator(original_images)
+
+# Preprocess original images
+original_images = original_images.detach().numpy()
+images = original_images - original_images.min(axis=0)
+images /= original_images.max(axis=0) - original_images.min(axis=0)
+original_images = np.transpose(images * 255.0, (0, 2, 3, 1)).astype(np.uint8)
+
+# Prepare outer grid on the plot
+fig = plt.figure(figsize=(10, 8))
+outer = gridspec.GridSpec(2, 1, wspace=0.2, hspace=0.2)
+
+# Plot all generated images
+ax = fig.add_subplot(outer[0])
+ax.set_xticks([])
+ax.set_yticks([])
+ax.axis('off')
+plt.setp(ax, title='Generated Images')
+inner = gridspec.GridSpecFromSubplotSpec(2, 8, subplot_spec=outer[0], wspace=0.1, hspace=0.1)
+for i in range(2*8):
+    ax = plt.Subplot(fig, inner[i])
+    ax.imshow(generated_images[i])
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.setp(ax, title='Fake' if generated_real_or_fake[i] > 0.5 else 'Real')
+    fig.add_subplot(ax)
+
+# Plot all original images
+ax = fig.add_subplot(outer[1])
+ax.set_xticks([])
+ax.set_yticks([])
+ax.axis('off')
+plt.setp(ax, title='Original Images')
+inner = gridspec.GridSpecFromSubplotSpec(2, 8, subplot_spec=outer[1], wspace=0.1, hspace=0.1)
+for i in range(2*8):
+    ax = plt.Subplot(fig, inner[i])
+    ax.imshow(original_images[i])
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.setp(ax, title='Fake' if original_real_or_fake[i] > 0.5 else 'Real')
+    fig.add_subplot(ax)
+
+# Save the figure
+fig.savefig(f'{configuration.output_figure}.png', format='png', dpi=300)
